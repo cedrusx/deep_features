95068a58f691e0bf8600d75bf27d385df4eff74b
Xuesong Shi <xuesong.shi@intel.com>
2020-07-30 12:30:48 +0800

    show_keypoints: print performance stats and add arg parsing
    Signed-off-by: Xuesong Shi <xuesong.shi@intel.com>

--- feature_extraction/show_keypoints.py
+++ feature_extraction/show_keypoints.py
@@ -9,34 +9,54 @@ import numpy as np
 import time
 import threading
 import sys
+import argparse
 
 def main():
+    parser = argparse.ArgumentParser(description='Test feature extraction and visualize keypoints')
+    parser.add_argument('--net', type=str, default='hfnet_vino',
+        help='Network model: hfnet_vino (default), hfnet_tf.')
+    parser.add_argument('--pause', action='store_true',
+        help='Wait for keyboard input after showing each image.')
+    parser.add_argument('--no-gui', action='store_true',
+        help='Do not visualize keypoints.')
+    args,filenames = parser.parse_known_args()
+
     net_name = 'hfnet_vino'
-    gui = True
-    if net_name == 'hfnet_vino':
-        from hfnet_vino import FeatureNet, default_config
-    elif net_name == 'hfnet_tf':
-        from hfnet_tf import FeatureNet, default_config
-    else:
-        exit('Unknown net %s' % net_name)
+    try:
+        cmd = 'from %s import FeatureNet, default_config' % (args.net)
+        exec(cmd, globals())
+    except ImportError as err:
+        exit('Unknown net %s: %s' % (net_name, str(err)))
     config = default_config
     #config['keypoint_threshold'] = 0
     net = FeatureNet(config)
-    filenames = sys.argv[1:]
+    frame_count = 0
+    total_time = 0.
+    # do an extra infer first because it may take much more time and mislead the performance analysis
+    if len(filenames) > 0:
+        net.infer(cv2.imread(filenames[0]))
     for f in filenames:
         image = cv2.imread(f)
         image = cv2.resize(image, (640, 480))
         start_time = time.time()
         features = net.infer(image)
         end_time = time.time()
+        frame_time = (end_time - start_time) * 1000
+        total_time += frame_time
+        frame_count += 1
         num_keypoints = features['keypoints'].shape[0]
         print(f + ': ' + str(image.shape) +
-                ', %d keypoints, %.2f ms' % (num_keypoints, (end_time - start_time) * 1000))
-        if gui:
-            draw_keypoints(image, features['keypoints'], features['scores'])
+                ', %d keypoints, %.2f ms (average %.2f ms)' % (num_keypoints, frame_time, total_time / frame_count))
+        if args.no_gui: continue
+        draw_keypoints(image, features['keypoints'], features['scores'])
+        if args.pause:
             title = f + ' (' + net_name + ', ' + str(num_keypoints) + ' keypoints)'
             cv2.imshow(title, image)
             cv2.waitKey()
+        else:
+            title = net_name
+            cv2.imshow(title, image)
+            cv2.waitKey(1)
 
 def draw_keypoints(image, keypoints, scores):
     upper_score = 0.2   # keypoints with this score or higher will have a red circle
